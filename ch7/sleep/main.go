8aa41155636da89108be0daa7b8d84aa501b2c4f
jiebin.cheng <jiebin.cheng@jiebin.cheng>
2021-11-12 18:32:39 +0800

    feat:ch7

--- ch7/sleep/main.go
+++ ch7/sleep/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -10,6 +11,10 @@ var period = flag.Duration("period", 1*time.Second, "sleep period")
 
 func main() {
 
+	var x interface{} = []int{1, 2, 3}
+
+	fmt.Printf("%T\n", x) // "<nil>"\
+	os.Exit(1)
 	flag.Parse()
 	fmt.Printf("Sleeping for %v...", *period)
 	time.Sleep(*period)
